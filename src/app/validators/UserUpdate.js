9e6142235b7b448657fc0e5fbc88024e3f8773e9
Renato Castro <renatocastro02@hotmail.com>
2020-08-11 13:30:00 -0300

    backend + express brute

---
+++ src/app/validators/UserUpdate.js
@@ -0,0 +1,27 @@
+import * as Yup from 'yup';
+
+export default async (req, res, next) => {
+  try {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(5),
+      password: Yup.string()
+        .min(5)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    await schema.validate(req.body, { aboutEarly: false });
+
+    return next();
+  } catch (err) {
+    return res
+      .status(400)
+      .json({ error: 'Validation fails', messages: err.inner });
+  }
+};
